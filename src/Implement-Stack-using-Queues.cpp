a52b54583df8d61028555f83e9312b3ed8846173
Finalcheat <finalcheat@qq.com>
2016-04-13 20:42:48 +0800

    Implement-Stack-using-Queues

---
+++ src/Implement-Stack-using-Queues.cpp
@@ -0,0 +1,56 @@
+/**
+ * @file Implement-Stack-using-Queues.cpp
+ * @brief 使用队列实现栈(https://leetcode.com/problems/implement-stack-using-queues/)
+ * @author Finalcheat
+ * @date 2016-04-13
+ */
+
+/**
+ * Implement the following operations of a stack using queues.
+ * * push(x) -- Push element x onto stack.
+ * * pop() -- Removes the element on top of the stack.
+ * * top() -- Get the top element.
+ * * empty() -- Return whether the stack is empty.
+ */
+
+/**
+ * 使用两个队列作辅助实现，push的时候将新元素加入到q2中，然后将q1的元素移到q2中，最后交换q1、q2，其他操作就是q1的正常操作即可。
+ */
+
+class Stack {
+    public:
+        // Push element x onto stack.
+        void push(int x) {
+            q2.push(x);
+            _q1MoveToQ2();
+            q1.swap(q2);
+        }
+
+        // Removes the element on top of the stack.
+        void pop() {
+            q1.pop();
+        }
+
+        // Get the top element.
+        int top() {
+            return q1.front();
+        }
+
+        // Return whether the stack is empty.
+        bool empty() {
+            return q1.empty();
+        }
+        
+    private:
+        void _q1MoveToQ2() {
+            while (!q1.empty()) {
+                int value = q1.front();
+                q1.pop();
+                q2.push(value);
+            }
+        }
+        
+    private:
+        std::queue<int> q1;
+        std::queue<int> q2;
+};
