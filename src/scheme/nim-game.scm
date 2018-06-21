604860fcb8ba5be4c6bdba670544b5cdfc9d71f7
Finalcheat <finalcheat@gmail.com>
2018-06-21 14:30:05 +0800

    nim-game.scm

---
+++ src/scheme/nim-game.scm
@@ -0,0 +1,28 @@
+;; 292. Nim游戏
+;; https://leetcode-cn.com/problems/nim-game/description/
+
+;; 你和你的朋友，两个人一起玩 Nim游戏：桌子上有一堆石头，每次你们轮流拿掉 1 - 3 块石头。
+;; 拿掉最后一块石头的人就是获胜者。你作为先手。
+;; 你们是聪明人，每一步都是最优解。 编写一个函数，来判断你是否可以在给定石头数量的情况下赢得游戏。
+
+;; 示例:
+;; 输入: 4
+;; 输出: false
+;; 解释: 如果堆中有 4 块石头，那么你永远不会赢得比赛；
+;; 因为无论你拿走 1 块、2 块 还是 3 块石头，最后一块石头总是会被你的朋友拿走。
+
+
+;; Guile
+
+(define (can-win-nim n)
+  (not (= (remainder n 4) 0)))
+
+
+;; Example1 => #f
+(can-win-nim 4)
+
+;; Example2 => #t
+(can-win-nim 5)
+
+;; Example3 => #t
+(can-win-nim 203)
