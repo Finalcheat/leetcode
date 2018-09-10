140eb4c1ad99eaa3b15a0493b93980efc22bbe7c
Finalcheat <finalcheat@gmail.com>
2018-09-10 20:30:18 +0800

    Construct-the-Rectangle.cpp

---
+++ src/cpp/Construct-the-Rectangle.cpp
@@ -0,0 +1,40 @@
+/**
+ * @file Construct-the-Rectangle.cpp
+ * @brief 构造矩形(https://leetcode.com/problems/construct-the-rectangle/description/)
+ * @author Finalcheat
+ * @date 2018-08-31
+ */
+
+/**
+ * For a web developer, it is very important to know how to design a web page's size. So, given a specific rectangular web page’s area, your job by now is to design a rectangular web page, whose length L and width W satisfy the following requirements:
+ * 1. The area of the rectangular web page you designed must equal to the given target area.
+ * 2. The width W should not be larger than the length L, which means L >= W.
+ * 3. The difference between length L and width W should be as small as possible.
+ * You need to output the length L and the width W of the web page you designed in sequence.
+ * Example:
+ * Input: 4
+ * Output: [2, 2]
+ * Explanation: The target area is 4, and all the possible ways to construct it are [1,4], [2,2], [4,1].
+ * But according to requirement 2, [1,4] is illegal; according to requirement 3,  [4,1] is not optimal compared to [2,2]. So the length L is 2, and the width W is 2.
+ */
+
+/**
+ * 如果可以开平方根，则sqrt就是最佳求，否则从sqrt得到的数开始作为width递减找到整除的数字length为所求。
+ */
+
+
+class Solution {
+    public:
+        vector<int> constructRectangle(int area) {
+            int width = std::sqrt(area);
+            int length = area / width;
+            while (width > 1 && width * length != area) {
+                --width;
+                length = area / width;
+            }
+            vector<int> result(2, 0);
+            result[0] = length;
+            result[1] = width;
+            return result;
+        }
+};
