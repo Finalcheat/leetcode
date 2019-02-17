052a60bf1687b4aa84a66cd2e140b433113074ed
Finalcheat <finalcheat@gmail.com>
2019-02-17 22:27:08 +0800

    Valid-Mountain-Array.cpp

---
+++ src/cpp/Valid-Mountain-Array.cpp
@@ -0,0 +1,49 @@
+/**
+ * @file Valid-Mountain-Array.cpp
+ * @brief 有效的山脉数组(https://leetcode.com/problems/valid-mountain-array)
+ * @author Finalcheat
+ * @date 2018-12-04
+ */
+
+/**
+ * Given an array A of integers, return true if and only if it is a valid mountain array.
+ * Recall that A is a mountain array if and only if:
+ * * A.length >= 3
+ * * There exists some i with 0 < i < A.length - 1 such that:
+ * * * A[0] < A[1] < ... A[i-1] < A[i]
+ * * * A[i] > A[i+1] > ... > A[B.length - 1]
+ * Example 1:
+ * Input: [2,1]
+ * Output: false
+ * Example 2:
+ * Input: [3,5,5]
+ * Output: false
+ * Example 3:
+ * Input: [0,3,2,1]
+ * Output: true
+ */
+
+/**
+ * 先升序遍历找到峰值，然后降序遍历，过程中发现不符合规则的直接返回false。
+ */
+
+
+class Solution {
+    public:
+        bool validMountainArray(vector<int>& A) {
+            if (A.size() < 3) {
+                return false;
+            }
+            size_t idx = 1;
+            while (idx < A.size() && A[idx - 1] < A[idx]) {
+                ++idx;
+            }
+            if (idx == 1 || idx == A.size()) {
+                return false;
+            }
+            while (idx < A.size() && A[idx - 1] > A[idx]) {
+                ++idx;
+            }
+            return idx == A.size();
+        }
+};
